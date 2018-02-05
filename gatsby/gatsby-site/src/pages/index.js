2f886157f883e482ccb43a28d931c581764a0386
tatsuroro <me@tatsuroro.com>
2018-02-05 16:40:32 +0900

    Add gatsby sandbox

---
+++ gatsby/gatsby-site/src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link><br/><br/>
+
+    <Link to="/pressrelease/test">pressrelease</Link><br/><br/>
+
+
+    <Link to="/info/test">info</Link>
+  </div>
+)
+
+export default IndexPage
