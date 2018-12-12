be050a458cac4b67f72d65938fd381cf24a2391b
Tatsuro Nakamura <me@tatsuroro.com>
2018-12-12 20:36:21 +0900

    chore

--- aws/s3meta/updatemeta.sh
+++ aws/s3meta/updatemeta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUCKETNAME=hugot-usercontent-development
-# KEY=files/c58e504f548442a195ca9d3ea5fea41d57d21509ab074bcaa75422f9c7268d227bd0d93a3f874733bcd58dc0eed0574c
+#TYPE KEY: 'files/c58e504f548442a195ca9d3ea5fea41d57d21509ab074bcaa75422f9c7268d227bd0d93a3f874733bcd58dc0eed0574c'
 
 for KEY in $(aws s3api list-objects \
   --bucket $BUCKETNAME \
